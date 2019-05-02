c23d5022aa8446a0cd3fde8e5377d9f5446cccee
josphat-kariuki <me_ziraq@yahoo.com>
2019-05-02 06:36:50 +0300

    Add specs PostsHelper methods

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,12 +1,11 @@
 module PostsHelper
-  def create_new_post_partial_path
 
+  def create_new_post_partial_path
     if user_signed_in?
       'posts/branch/create_new_post/signed_in'
     else
       'posts/branch/create_new_post/not_signed_in'
     end
-
   end
 
   def all_categories_button_partial_path
