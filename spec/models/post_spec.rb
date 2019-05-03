97f74f18718e28d90590f58323d0e25f7701c0ec
josphat-kariuki <me_ziraq@yahoo.com>
2019-05-03 12:07:03 +0300

    Commented out default_scope in post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,12 +16,41 @@ RSpec.describe Post, type: :model do
 
   end
 
-  # context 'Scopes' do
-  #   it 'default_scope orders by descending created_at' do
-  #     first_post = create(:post)
-  #     second_post = create(:post)
-  #     expect(Post.all).to eq [second_post, first_post]
-  #   end
-  # end
-  
+  context 'Scopes' do
+    
+    # it 'default_scope orders by descending created_at' do
+    #   first_post = create(:post)
+    #   second_post = create(:post)
+    #   expect(Post.all).to eq [second_post, first_post]
+    # end
+
+    it "by_category posts scope gets post by particular category" do
+      category = create(:category)
+      create(:post, category_id: category.id)
+      create_list(:post, 10)
+      posts = Post.by_category(category.branch, category.name)
+      expect(posts.count).to eq 1
+      expect(posts[0].category.name).to eq category.name
+    end
+
+    it "by_branch scope gets posts by a particular branch" do
+      category = create(:category)
+      create(:post, category_id: category.id)
+      create_list(:post, 10)
+      posts = Post.by_branch(category.branch)
+      expect(posts.count).to eq 1
+      expect(posts[0].category.branch).to eq category.branch
+    end
+
+    it "search scope finds a marching post" do
+      post = create(:post, title: "awesome title", content: 'great content' * 5)
+      create_list(:post, 10, title: ('a'..'b' * 2).to_a.shuffle.join)
+      expect(Post.search('awesome').count).to eq 1
+      expect(Post.search('awesome')[0].id).to eq post.id
+      expect(Post.search('great').count).to eq 1
+      expect(Post.search('great')[0].id).to eq post.id
+    end
+
+  end
+
 end
